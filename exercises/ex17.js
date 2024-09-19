c6137813a0f71edfd76a596ecb48305de2f0266b
Philip Yaw Boahene <boahenep22@gmail.com>
2024-09-19 13:34:27 -0700

    exercise 17 completed

--- exercises/ex17.js
+++ exercises/ex17.js
@@ -4,3 +4,19 @@ Write an anonymous function expression that stores a function in a variable call
 laugh(3);
 Returns: hahaha!
 */
+
+
+function laughter(times) {
+
+    let laughter = "";
+    for(let x = 1; x <= times; x++) {
+        laughter = laughter + "ha"
+    } 
+    
+    console.log(`${laughter}!`)
+
+    // return laughter    
+} 
+    // console.log(laugh())
+    
+laughter(3)
