e094f3954b9801f1fba4f6ed2ace53bfe2144b67
Philip Yaw Boahene <boahenep22@gmail.com>
2024-09-22 20:09:22 -0700

    exercise 26 completed

--- exercises/ex26.js
+++ exercises/ex26.js
@@ -10,6 +10,6 @@ const breakfast = {
     name: "The Lumberjack",
     price: 9.95,
     ingredients: ["eggs", "sausage", "toast", "hashbrowns", "pancakes"]
-};
+}
 
-console.log(breakfast);
+console.log(breakfast)
